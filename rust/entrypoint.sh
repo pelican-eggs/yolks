3449a5c5ca339be2dc4e368d20f264c8d2014650
hongbangchu <120215553+hongbangff@users.noreply.github.com>
2024-11-23 21:30:29 +0700

    Update entrypoint.sh

--- rust/entrypoint.sh
+++ rust/entrypoint.sh
@@ -5,12 +5,18 @@ cd /home/container
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-# Print current Rust version
-cargo --version
-
 # Replace Startup Variables
-MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Run the Server
-eval ${MODIFIED_STARTUP}
+# Run the Server in background and wait until it's ready
+eval ${MODIFIED_STARTUP} &
+
+# Wait for the game server to initialize
+while ! ps aux | grep '[S]tardewValleys'; do
+    echo "Waiting for StardewValleys to start..."
+    sleep 2
+done
+
+# Notify Pterodactyl that the server is online
+echo "StardewValleys is now online!"
